041af7895a68b07dc885f69696833f4e46b23f2c
Mesilane <106611362+Mesilane@users.noreply.github.com>
2023-03-01 20:24:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let cells = document.querySelectorAll('#field td');
+
+function start(cells) {
+    let i = 0;
+    for (let cell of cells) {
+        cell.addEventListener('click', function step() {
+            this.textContent = ['X', 'O'][i % 2];
+            this.removeEventListener('click', step());
+
+
+            i++;
+        })
+        break;
+    }
+
+}
+
+start(cells);
