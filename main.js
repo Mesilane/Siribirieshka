5dee80100adc242122e5fd7ed879eabe49e0f096
Mesilane <106611362+Mesilane@users.noreply.github.com>
2023-03-09 18:51:26 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 let cells = document.querySelectorAll('#field td');
 
+function victory(cells) {
+    let combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent === cells[comb[1]].textContent &&
+            cells[comb[1]].textContent === cells[comb[2]].textContent &&
+            cells[comb[0]].textContent !== '') {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 function start(cells) {
     let i = 0;
     for (let cell of cells) {
         cell.addEventListener('click', function step() {
-            this.textContent = ['X', 'O'][i % 2];
+            if (i % 2 == 0) {
+                this.textContent = 'X';
+            } else {
+                this.textContent = 'O';
+            };
             this.removeEventListener('click', step);
+            if (victory(cells)) {
+                alert('WINS ' + this.textContent)
+            } else if (i === 8) {
+                alert('DRAW')
+            }
+
             i++;
         })
-        break;
     }
+    const turn = i;
 }
 
 start(cells);
